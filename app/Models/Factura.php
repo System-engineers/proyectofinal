d776d73bbb3ed2f6f96eda793300f3dae7f7f653
AngelaArrieta17 <arrietaangela@hotmail.com>
2019-11-24 18:01:54 -0500

    Creacion de modelos y seeder

---
+++ app/Models/Factura.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Factura extends Model
+{
+    protected $table = 'facturas';
+
+    protected $fillable = [
+        'total', 'abono', 'saldo', 'estado', 'orden_id'
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
+    protected $casts = ['estado' => 'boolean'];
+    
+    public function orden()
+    {
+        return $this->belongsTo('App\Models\Orden');
+    }
+
+    public function empleado_orden()
+    {
+        return $this->belongsToMany('App\Models\EmpleadoOrden','detalle_factura')->withPivot('valor')->withTimestamps();
+    }
+
+
+}
