7e74fb04b6b77b2d6b3f1f13630526d9125690bd
CarolaineHerrera <carolainedayan.12@gmail.com>
2019-11-24 18:16:06 -0500

    Creacion de migraciones y faker

---
+++ database/migrations/2019_11_24_170011_create_categorias_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriasTable extends Migration
+{
+   
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre', 50);
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+}
